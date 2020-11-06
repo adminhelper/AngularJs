8a85dd79feed5ff2ad665161dba7b69d66b22006
adminhelper <201401339hs@gmail.com>
2020-11-06 14:56:55 +0900

    form item

---
+++ listapp/item/script.js
@@ -0,0 +1,62 @@
+var app = angular.module("todo", []);
+
+app.controller("TodoCtrl", function ($scope) {
+  $scope.todos = [
+    {
+      title: "요가수련",
+      completed: false,
+      createdAt: Date.now(),
+    },
+    {
+      title: "요가 수행",
+      completed: false,
+      createdAt: Date.now(),
+    },
+    {
+      title: "요가 완료",
+      completed: true,
+      createdAt: Date.now(),
+    },
+  ];
+
+  $scope.remove = function (todo) {
+    // find todo index in todos
+    var idx = $scope.todos.findIndex(function (item) {
+      return item.id === todo.id;
+    });
+
+    // if remove form todos
+    if (idx > -1) {
+      alert("삭제하겠습니까?");
+      $scope.todos.splice(idx, 1);
+    }
+  };
+
+  $scope.add = function (newTodoTitle) {
+    // create new todo
+    var newTodo = {
+      title: newTodoTitle,
+      completed: false,
+      createdAt: Date.now(),
+    };
+
+    // push into todos
+    $scope.todos.push(newTodo);
+
+    //empty form
+    $scope.newTodoTitle = "";
+    alert(newTodoTitle);
+  };
+});
+
+app.directive("todoTitle", function () {
+  return {
+    template: "<h1> 목록 </h1>",
+  };
+});
+
+app.directive("todoItem", function () {
+  return {
+    templateUrl: "todoItem.tpl.html",
+  };
+});
