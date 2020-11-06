49ac7d6e086ff5b877a69e5923c162831b4d233e
adminhelper <201401339hs@gmail.com>
2020-11-06 10:14:00 +0900

    ngRepeat study

--- Expression/script.js
+++ Expression/script.js
@@ -1,21 +1,21 @@
-var app = angular.module('todo', []);
+var app = angular.module("todo", []);
 
-app.controller('TodoCtrl', function($scope) {
-    $scope.todos = [
-        {
-            title:'요가수련',
-            completed: false,
-            createdAt : Date.now()
-        },
-        {
-            title:'요가 수행',
-            completed: false,
-            createdAt : Date.now()
-        },
-        {
-            title:'요가 완료',
-            completed: true,
-            createdAt : Date.now()
-        }
-    ]
+app.controller("TodoCtrl", function ($scope) {
+  $scope.todos = [
+    {
+      title: "요가수련",
+      completed: false,
+      createdAt: Date.now(),
+    },
+    {
+      title: "요가 수행",
+      completed: false,
+      createdAt: Date.now(),
+    },
+    {
+      title: "요가 완료",
+      completed: true,
+      createdAt: Date.now(),
+    },
+  ];
 });
